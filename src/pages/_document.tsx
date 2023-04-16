44ab17cf08c270e8943df7c7e92fa7802a68f829
giovani.franz <giovani.franz@agi.com.br>
2023-04-16 10:08:44 -0300

    feat/mudança do _document para componente funcional

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,19 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="pt-BR" className="scroll-smooth antialiased" color="#202020">
-        <Head>
-          <link rel="canonical" href="https://www.giovanifranz.dev" />
-          <meta
-            name="description"
-            content="Um desenvolvedor front-end com experiência em desenvolvimento de software especializado em React e NodeJS. Tenho experiência em front-end e back-end."
-          />
-        </Head>
-        <body className="overflow-x-hidden bg-black text-white">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+export default function Document() {
+  return (
+    <Html lang="pt-BR" className="antialiased" color="#202020">
+      <Head>
+        <link rel="canonical" href="https://www.giovanifranz.dev" />
+        <meta
+          name="description"
+          content="Um desenvolvedor front-end com experiência em desenvolvimento de software especializado em React e NodeJS. Tenho experiência em front-end e back-end."
+        />
+      </Head>
+      <body className="overflow-x-hidden scroll-smooth bg-black text-white">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
